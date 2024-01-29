515d406a789a31808aab6dc926fd1b520fd07cc4
noload <vaibhavkamble023@gmail.com>
2024-01-29 23:24:22 +0530

    added tailwind configuration setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,6 @@
-
+import "./app.css";
 function App() {
-
-  return (
-    <div>
-        Hello world
-    </div>
-  )
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
 }
 
-export default App
+export default App;
