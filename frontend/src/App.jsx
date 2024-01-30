33675cacd6c037fbba0a3f931498e0e7f94f681d
noload <vaibhavkamble023@gmail.com>
2024-01-30 15:02:16 +0530

    added sign signup and basic component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,22 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./app.css";
+import Signup from "./component/Signup";
+import Signin from "./component/Signin";
+import Dashboard from "./component/Dashboard";
+import SendMoney from "./component/SendMoney";
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/send" element={<SendMoney />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
